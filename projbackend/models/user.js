f68d6a978aa471d1692f5d323d0ea7bb8e3a4a0f
Vivek <mailmekvk@gmail.com>
2020-07-09 22:32:21 +0530

    User Schema Added

---
+++ projbackend/models/user.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const uuidv1 = require('uuid/v1');
+
+var userSchema = new mongoose.Schema({
+    firstName :{
+        type:String,
+        required:true,
+        maxlength:32,
+        trim:true
+    },
+    lastName:{
+        type:String,
+        maxlength:32,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    encryptPassword:{
+        type:String,
+        required:true
+    },
+
+    phoneNumber:{
+        type:Number,
+        maxlength:10,
+        trim:true,
+    },
+    userInfo:{
+        type:String,
+        trim:true
+    },
+
+    salt:String,
+    role:{
+        type:Number,
+        default:0
+    },
+    purchases:{
+        type:Array,
+        default:[]
+    }
+});
+
+userSchema.virtual("password")
+    .set(function(password){
+        this._password = password;
+        this.salt = uuidv1();
+        this.encryptPassword = this.securePassword(password);
+
+    })
+    .get(function(){
+        return this._password;
+    });
+
+
+userSchema.methods = {
+
+    authenticate: function(plainPassword){
+        return this.securePassword(plainPassword) === this.encryptPassword;
+    },
+
+    securePassword: function(plainPassword){
+        if(!plainPassword)
+            return "";
+        try {
+            return crypto.createHmac('sha256', this.salt)
+            .update(plainPassword)
+            .digest('hex'); 
+        } catch (err) {
+            return "";
+        }
+    }
+};
+
+module.exports = mongoose.model("User",userSchema);
