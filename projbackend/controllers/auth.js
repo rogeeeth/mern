2edc3c8ac13606e68235bbcbf2e80d6c86f9116c
Vivek <mailmekvk@gmail.com>
2020-07-13 23:51:51 +0530

    Auhtorization through JWT Added

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,6 +1,42 @@
 const User = require('../models/user');
 const { check, validationResult } = require('express-validator');
+const { json } = require('body-parser');
+var expressJwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
+const user = require('../models/user');
+require('dotenv').config()
 
+exports.login = (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error:errors.array()
+        });
+    }
+
+    const {email, password} = req.body;
+    User.findOne({email},(err,user)=>{
+        if(err || !user){
+            return res.status(400).json({
+                error:"User does not exist"
+            });
+        }
+        
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error:"Email and password does not match"
+            });
+        }
+
+        const token = jwt.sign({_id: user._id},process.env.JWTSECRET);
+        res.cookie("token",token, {expire: new Date()+ 9999} );
+
+        const {email:userEmail,_id:userId,firstName:userFirstName, role:userRole} = user;
+        return res.json({token, user:{_id:userId, firstName:userFirstName,email:userEmail,role:userRole}});
+        
+    })
+
+};
 exports.signup = (req,res)=>{
 const errors = validationResult(req);
 console.log(errors);
@@ -25,6 +61,35 @@ user.save((err,user)=>{
     }
 });
 };
+
 exports.signout = (req,res)=>{
-    res.send('Bye!');
+    res.clearCookie("token");
+    res.json({
+        message:"User Signed Out"
+    });
+};
+
+
+exports.isLoggedIn = expressJwt({
+    secret:process.env.JWTSECRET,
+    userProperty:"auth"
+});
+
+exports.isAuthorized= (req,res,next)=>{
+
+    let checker = req.profile&&req.auth&&req.profile._id==req.auth._id;
+    if(!checker){
+        return res.status(403).json({
+            error:"ACCESS DENIED"
+        });
+    }
+    next();
+};
+exports.isAdmin= (req,res,next)=>{
+    if(req.profile.role ===0){
+        return res.status(403).json({
+            error:"NOT AN ADMIN, ACCESS DENIED"
+        });
+    }
+    next();
 };
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-const {signout,signup} = require('../controllers/auth');
+const {signout,signup,login,isLoggedIn} = require('../controllers/auth');
 
 router.post('/signup',[
     check("email").isEmail().withMessage('Email is required'),
     check("password").isLength({ min: 8}).withMessage('Password must have minimum of 8 characters')
 ],signup);
+router.post('/login',[
+    check("email").isEmail().withMessage('Email is required'),
+    check("password").isLength({ min: 8}).withMessage('Password must have minimum of 8 characters')
+],login);
 router.get('/signout',signout);
+
 
 module.exports=router;
