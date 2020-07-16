3a9efc937f65ff1b76d01b4d8b9539fb28004719
Vivek <mailmekvk@gmail.com>
2020-07-17 01:12:01 +0530

    Product Methods Added

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -2,6 +2,7 @@ const Product = require('../models/product');
 const formidable = require('formidable');
 const _ = require('lodash');
 const fs = require('fs');
+const product = require('../models/product');
 
 
 
@@ -68,4 +69,112 @@ exports.getFile = (req,res,next)=>{
     }
 
     next();
-}
+};
+
+exports.deleteProduct = (req,res)=>{
+    let product =  req.product;
+
+    product.remove((err,deletedProduct)=>{
+        if(err){
+            return res.status(400).json({
+                error: "Failed to Delete Product"
+            });
+        }
+        res.json({
+            message: "Deletion Successfull",
+            deleteProduct
+        });
+    });
+};
+
+exports.updateProduct = (req,res)=>{
+
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+
+    form.parse(req,(err,fields,file)=>{
+        if(err){
+            return res.status(400).json({
+                error:"Corrupted Image"
+            });
+        }
+
+        let product = req.product;
+        product = _.extend(product,fields);
+
+        if(file.photo){
+            if(file.photo.size > 3000000){
+                return res.status(400).json({
+                    error:"File Size Too Big"
+                });
+            }
+            product.photo.data = fs.readFileSync(file.photo.path);
+            product.photo.contentType = file.photo.type;
+        }
+
+        product.save((err,product)=>{
+            if(err){
+                res.status(400).json({
+                    error: "Updation of Product Failed"
+                });
+            };
+            res.json(product);
+        });
+    });
+};
+
+
+exports.getAllProducts = (req,res)=>{
+    let limit =  req.query.limit ? parseInt(req.query.limit) : 10;
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    Product.find()
+    .select("-photo")
+    .populate("category")
+    .sort([[sortBy, "asc"]])
+    .limit(limit)
+    .exec((err,products)=>{
+        if(err || !products){
+            return res.status(400).json({
+                error:"No Product Found"
+            });
+        }
+        res.json(products);
+    })
+
+};
+
+
+exports.updateInventory = (req,res,next) =>{
+
+    let myOperations =  req.body.order.products.map(product =>{
+            return {
+                updateOne : {
+                    filter: {_id: product._id},
+                    update: {$inc :{stock: -product.count,sold: +product.count}}
+                }
+            }
+    });
+
+    Product.bulkWrite(myOperations, {}, (err,result)=>{
+        if(err){
+            res.status(400).json({
+                error:"Bulk Operation Failed"
+            });
+        }
+        res.json({
+            message: "Operation Successfull"
+        })
+    });
+    next();
+};
+
+exports.getAllUniqueCategories = (req,res)=>{
+    Product.distinct("category",(err,categories)=>{
+    if(err){
+        return res.status(400).json({
+            error:"No Categories Found"
+        });
+    }
+    res.json(categories);
+    });
+};
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {getProductById,createProduct,getProduct,getFile} = require('../controllers/product');
+const {getProductById,createProduct,getProduct,getFile,deleteProduct,updateProduct,getAllProducts,getAllUniqueCategories} = require('../controllers/product');
 const {getUserById} = require('../controllers/user');
 const {isLoggedIn,isAuthorized,isAdmin} = require('../controllers/auth');
 
@@ -9,7 +9,12 @@ router.param('userId',getUserById);
 router.param('productId',getProductById);
 
 router.post('/product/create/:userId',isLoggedIn,isAuthorized,isAdmin,createProduct);
+router.delete('/product/:productId/:userId',isLoggedIn,isAuthorized,isAdmin,deleteProduct);
+router.put('/product/:productId/:userId',isLoggedIn,isAuthorized,isAdmin,updateProduct);
 router.get('/product/:productId',getProduct);
+router.get('/products',getAllProducts);
 //router.get("/product/photo/:productId",getFile);
+
+router.get("/products/categories",getAllUniqueCategories);
 
 module.exports = router;
