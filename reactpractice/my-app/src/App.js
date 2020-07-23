3a2d8b331ceb43953dc7af72e4319b78c44a9a81
Vivek <mailmekvk@gmail.com>
2020-07-23 23:56:03 +0530

    MERN Todo App

--- reactpractice/my-app/src/App.js
+++ reactpractice/my-app/src/App.js
@@ -5,21 +5,57 @@ import './App.css';
 class App extends React.Component{
   constructor(props){
     super(props);
-    this.state = {courses:['React','Node','Express'],
-      currentInput:""};
+    this.state = {courses:[],
+    newItem:""};
+      this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(inputValue){
+    if(inputValue !==""){
+    const newItem = {
+      id:inputValue+'-'+Date.now(),
+      value:inputValue,
+      isDone:false
+    }
+    let list = [...this.state.courses];
+    list.push(newItem);
+    this.setState({
+      courses:list,
+      newItem:""
+    });
+  }
+
+  }
+
+  deleteItem(id){
+    let list = [...this.state.courses];
+    let updatedList = list.filter((item)=>item.id!==id);
+    this.setState({
+      courses:updatedList
+    });
+  }
+
+  newInput(input){
+    this.setState({
+      newItem:input
+    });
   }
 
   render(){
     return(
-      <div class="App">
+      <div className="App">
       <h1>Courses to Learn</h1>
-      <input type="Text"/>
-      <button>Add to List</button>
+      <input type="Text" required value={this.state.newItem} onChange={e =>this.newInput(e.target.value)}/>
+      <button onClick={()=>this.addItem(this.state.newItem)} disabled={!this.state.newItem.length}>Add to List</button>
+      <ul>
       {this.state.courses.map(course =>{
         return(
-        <p>{course}</p>
+
+        <li key={course.id}><input type="checkbox"/>{course.value}<button onClick={e=>{this.deleteItem(course.id)}}>Delete</button></li>
+  
         )
       })}
+      </ul>
       </div>
      
     )
